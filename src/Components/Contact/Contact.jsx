93dad0e94e75e449147fbf96488431de23ca2695
Luan Mota Coleto <luancoleto@outlook.com>
2020-04-28 03:36:29 +0000

    Contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -13,19 +13,20 @@ class Contact extends Component{
                     </div>
                     <div className="row">
                         <div className="col-md-12 mb-5">
-                            <form className=" ">
-
+                            <form className=" " action="https://api.staticforms.xyz/submit" method="post">
+                                <input type="hidden" name="accessKey" value="2541623f-fdf9-45bf-93fa-93892e56264f"/>
+                                <input type="hidden" name="subject" value="Mensagem do site" />
                                 <div  className="row form-group">
                                     <div className="col-md-12">
-                                        <label className="text-white label-text" htmlFor="fname">Nome</label>
-                                        <input type="text" id="fname"  className="form-control"/>
+                                        <label className="text-white label-text">Nome</label>
+                                        <input type="text"  name="name" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="row form-group">
                                     <div className="col-md-12">
                                         <label className="text-white label-text" htmlFor="email">E-mail</label>
-                                        <input type="email" id="email" className="form-control"/>
+                                        <input type="email"  name="email"  className="form-control"/>
                                     </div>
                                 </div>
 
@@ -33,13 +34,14 @@ class Contact extends Component{
                                 <div className="row form-group">
                                     <div className="col-md-12">
                                         <label className="text-white label-text " htmlFor="mensage">Mensagem</label>
-                                        <textarea  name="message" id="message" cols="30" rows="7" className="form-control" />
+                                        <textarea  name="message" name="message" cols="30" rows="7" className="form-control" />
                                     </div>
                                 </div>
-
+                                <input type="hidden" name="replyTo" value="luancoleto@outlook.com"/>
+                                <input type="text" name="honeypot" style={{display: 'none'}}></input>
                                 <div className="row form-group">
                                     <div className="col-md-12">
-                                        <input type="submit" value="Enviar mensagem" className="btn py-2 px-4 text-white label-text"/>
+                                        <input type="submit" value="Enviar mensagem" className="btn py-2 px-4 text-white label-text" id="submit-btn"/>
                                     </div>
                                 </div>
 
