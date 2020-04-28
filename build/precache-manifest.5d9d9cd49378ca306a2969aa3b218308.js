9480c1f9498aa2f7e1f183019561c7cab9a1bd81
Luan Mota Coleto <luancoleto@outlook.com>
2020-04-28 20:57:58 +0000

    gitignore

---
+++ build/precache-manifest.5d9d9cd49378ca306a2969aa3b218308.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d741c66ccd8abc624111d2db0d21c547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab3dbcd40810199db26b",
+    "url": "/static/css/2.69580acf.chunk.css"
+  },
+  {
+    "revision": "d755b1cc888dc27e3c66",
+    "url": "/static/css/main.d524fea1.chunk.css"
+  },
+  {
+    "revision": "ab3dbcd40810199db26b",
+    "url": "/static/js/2.f6bb40ed.chunk.js"
+  },
+  {
+    "revision": "39ee42a9c6b75cfef987502f097de4c8",
+    "url": "/static/js/2.f6bb40ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d755b1cc888dc27e3c66",
+    "url": "/static/js/main.193859b2.chunk.js"
+  },
+  {
+    "revision": "a0cfd62bb9dc27ab8442",
+    "url": "/static/js/runtime-main.470a306c.js"
+  },
+  {
+    "revision": "8fa2a61f65436e9df69d5c38d590cbf8",
+    "url": "/static/media/001-food.8fa2a61f.svg"
+  },
+  {
+    "revision": "029ae1098b0121f91f7bf3c63ccc48fa",
+    "url": "/static/media/001-github.029ae109.svg"
+  },
+  {
+    "revision": "9a894325c337a8e6e46746d7fd41353c",
+    "url": "/static/media/002-linkedin.9a894325.svg"
+  },
+  {
+    "revision": "0ad837fcfdd85a29eaf559df02c3d17d",
+    "url": "/static/media/002-project.0ad837fc.svg"
+  },
+  {
+    "revision": "e4c25de42623633b3496afca3ce01e06",
+    "url": "/static/media/003-code.e4c25de4.svg"
+  },
+  {
+    "revision": "c36e510fa69a58b93005f48f0a330605",
+    "url": "/static/media/003-whatsapp.c36e510f.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/static/media/004-heart.7746f5e4.svg"
+  },
+  {
+    "revision": "83ebdff0fdead58b5fa710bcde707809",
+    "url": "/static/media/development-banner.83ebdff0.jpeg"
+  },
+  {
+    "revision": "0834584a5da127668fd06089b9477355",
+    "url": "/static/media/luan-coleto.0834584a.jpg"
+  }
+]);
