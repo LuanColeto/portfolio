db0b793bd75362fa4e5def4966579bd5aee19b20
Luan Coleto <luancoleto@outlook.com>
2020-05-27 11:00:36 -0300

    Fixed navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import $ from 'jquery';
 import './Cabecalho.css';
 
 
@@ -7,7 +6,7 @@ class Navbar extends Component{
     render() {
             return(
                 <header id="navbar">
-                    <nav className="navbar navbar-expand-md navbar-dark col-md-12"  style={{background: '#133B57'}} >
+                    <nav className="navbar fixed-top navbar-expand-md navbar-dark col-md-12"  style={{background: '#133B57'}} >
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
                                 aria-label="Toggle navigation">
@@ -15,17 +14,20 @@ class Navbar extends Component{
                         </button>
                         <div className="collapse navbar-collapse" id="navbarCollapse">
                             <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <a className="nav-link"  href="#">Início <span className="sr-only">(current)</span></a>
+                                <li className="nav-item">
+                                    <a className="nav-link"  href="#navbar">Início <span className="sr-only">(current)</span></a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Sobre</a>
+                                    <a className="nav-link" href="#section-about">Sobre</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link " href="#">Projetos</a>
+                                    <a className="nav-link" href="#Skills">Habilidades</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link " href="#">Contato</a>
+                                    <a className="nav-link " href="#projects-section">Projetos</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link " href="#contact-section">Contato</a>
                                 </li>
                             </ul>
                         </div>
