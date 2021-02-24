3003868f8bb99f89ac5cf649c4619cacb5db64db
Luan Mota Coleto <luancoleto@outlook.com>
2021-02-24 09:41:30 -0300

    Add skills section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,8 +77,331 @@ export default function Home() {
         </div>
       </section>
 
+      <div className="position-relative mt-10">
+        <div className="shape shape-bottom shape-fluid-x svg-shim text-dark">
+          <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z" fill="currentColor"/>
+          </svg>
+        </div>
+      </div>
+
+      <section className="py-8 pt-md-11 pb-md-12 bg-dark">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-8 text-center">
+
+              {/*<h6 className="text-uppercase text-success fw-bold">*/}
+              {/*  All in one solution*/}
+              {/*</h6>*/}
+
+              <h1 className="fw-bold text-white">
+                Minhas habilidades.
+              </h1>
+
+              <p className="fs-lg text-gray-500 mb-7 mb-md-9">
+                Veja todas as minhas competências técnicas.
+              </p>
+
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-3 mb-4">
+
+              <h4 className="fw-bold text-white mb-5">
+                Linguagens
+              </h4>
+
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  TypeScript
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  JavaScript
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Node js
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Ruby
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  HTML
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  CSS
+                </p>
+
+              </div>
+
+            </div>
+            <div className="col-12 col-md-6 col-lg-3 mb-4">
+
+              <h4 className="fw-bold text-white mb-5">
+                Frameworks
+              </h4>
+
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Express js
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Adonis js
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  React
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  React native
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Next js
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Ruby on rails
+                </p>
+
+              </div>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-3 mb-4">
+
+              <h4 className="fw-bold text-white mb-5">
+                Banco de dados
+              </h4>
+
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  PostgreSQL
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  MySQL
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  SQLite
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  MongoDB
+                </p>
+
+              </div>
+
+            </div>
+            <div className="col-12 col-md-6 col-lg-3 mb-4">
+
+              <h4 className="fw-bold text-white mb-5">
+                Ferramentas
+              </h4>
+
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  AWS
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Digital ocean
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Docker
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Netlify
+                </p>
+
+              </div>
+              <div className="d-flex">
+
+                <div className="badge badge-rounded-circle bg-success mt-1 me-4">
+                  <i className="fe fe-check"></i>
+                </div>
+
+                <p className="text-gray-300">
+                  Vercel
+                </p>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-4 pt-md-11 mt-11">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-12 col-md-5 col-lg-6 order-md-2" style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+
+              <img src="/code-intro.svg"
+                   className="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="..." data-aos="fade-up"
+                   data-aos-delay="100" />
 
 
+            </div>
+            <div className="col-12 col-md-7 col-lg-6 order-md-1" data-aos="fade-up">
+
+              <h1 className="display-3 text-center text-md-start">
+                Sobre mim
+              </h1>
+
+
+              <p className="lead text-center text-md-start text-muted mb-lg-8">
+                Oi! Meu nome é Luan Mota Coleto, sou um desenvolvedor de software, apaixonado não só por programação, mas pelo o que eu posso fazer com ela. Tenho como propósito resolver problemas, através de código.
+              </p>
+
+              <p className="lead text-center text-md-start text-muted mb-6 mb-lg-8">
+                Tenho experiência em projetos de médio porte, tanto mobile quanto web, construindo tanto o lado do servidor quanto o lado do cliente (apesar de ter uma paixão por back-end). Minha Stack principal é React e node, além de trabalhar com Ruby e Rails, mas sempre buscando aprender coisas novas.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </section>
 
 
     </>
